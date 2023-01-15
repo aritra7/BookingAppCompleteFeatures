27ecc4377768cce53364e1a87d91c5ad995d9e7f
aritra7 <kunduaritra7@gmail.com>
2023-01-16 00:06:21 +0530

    axios put method added with edit button

--- bookingappcompletefeatures.js
+++ bookingappcompletefeatures.js
@@ -10,7 +10,7 @@ function saveMe(event){
     // localStorage.setItem(userDetails.email,JSON.stringify(userDetails))
     // showUserOnList(userDetails)
     // To save user details to the cloud storage / crud crud and show
-    axios.post('https://crudcrud.com/api/01d3733d06b14a139325aae2e5514053/AppoinmentData',userDetails)
+    axios.post('https://crudcrud.com/api/2f4646366ae84572b667c04f4fc81612/AppoinmentData',userDetails)
     .then((response)=>showUserOnList(response.data))
     .catch((err)=>document.body.innerHTML += `${err}`)
 }
@@ -18,7 +18,7 @@ function showUserOnList(users){
     document.getElementById('username').value =''
     document.getElementById('email').value =''
     document.getElementById('contact').value =''
-    const li = `<li class="list-group-item" id='${users._id}'>${users.username} : ${users.email} <button class="btn btn-success btn-sm" onclick=editinfo('${users.username}','${users.email}','${users.contactNo}')>Edit</button> <button class="btn btn-danger btn-sm" onclick=deleteinfo('${users._id}')>Delete</button></li>`
+    const li = `<li class="list-group-item" id='${users._id}'>${users.username} : ${users.email} <button class="btn btn-success btn-sm" onclick=editinfo('${users.username}','${users.email}','${users.contactNo}','${users._id}')>Edit</button> <button class="btn btn-danger btn-sm" onclick=deleteinfo('${users._id}')>Delete</button></li>`
     const parentNode = document.getElementById('listofusers')
     parentNode.innerHTML += li
 }
@@ -32,7 +32,7 @@ window.addEventListener("DOMContentLoaded",()=>{
     //     const keyDetailsObj = JSON.parse(keyDetails)
     //     showUserOnList(keyDetailsObj)
     // }
-    axios.get("https://crudcrud.com/api/01d3733d06b14a139325aae2e5514053/AppoinmentData")
+    axios.get("https://crudcrud.com/api/2f4646366ae84572b667c04f4fc81612/AppoinmentData")
     .then((response)=>{
         for(let i=0; i<response.data.length; i++){
             let key = response.data[i];
@@ -41,18 +41,26 @@ window.addEventListener("DOMContentLoaded",()=>{
     })
     .catch((err)=>document.body.innerHTML +=`Error: ${err}`)
 })
-function editinfo(editNameInfo, editEmailInfo, editContactInfo){
+function editinfo(editNameInfo, editEmailInfo, editContactInfo,hashvalue){
     document.getElementById('username').value = editNameInfo;
     document.getElementById('email').value = editEmailInfo;
     document.getElementById('contact').value = editContactInfo;
-    deleteinfo(editEmailInfo)
+    axios.put(`https://crudcrud.com/api/2f4646366ae84572b667c04f4fc81612/AppoinmentData/${hashvalue}`,
+        {
+            "username": `${editNameInfo}`,
+            "email": `${editEmailInfo}`,
+            "contactNo": `${editContactInfo}`
+        }
+    )
+    .then((response)=>deleteinfo(hashvalue))
+    .catch((err)=>document.body.innerHTML+=`Error: ${err}`)
 }
 function deleteinfo(delUserInfo){
     // console.log(delUserInfo)
     // localStorage.removeItem(delUserInfo);
-    axios.delete(`https://crudcrud.com/api/01d3733d06b14a139325aae2e5514053/AppoinmentData/${delUserInfo}`)
+    axios.delete(`https://crudcrud.com/api/2f4646366ae84572b667c04f4fc81612/AppoinmentData/${delUserInfo}`)
     .then((response)=>removeUserInfoFromScreen(delUserInfo))
-    .catch((err)=document.body.innerHTML+=`${err}`)
+    .catch((err)=>document.body.innerHTML+=`${err}`)
 }
 function removeUserInfoFromScreen(usr){
     const parentNode = document.getElementById('listofusers')
