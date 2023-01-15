02874d2bc706cf2b66a8d6958fe5678678d4c1cd
aritra7 <kunduaritra7@gmail.com>
2023-01-15 22:28:03 +0530

    added axios delete to delete a user

--- bookingappcompletefeatures.js
+++ bookingappcompletefeatures.js
@@ -18,7 +18,7 @@ function showUserOnList(users){
     document.getElementById('username').value =''
     document.getElementById('email').value =''
     document.getElementById('contact').value =''
-    const li = `<li class="list-group-item" id='${users.email}'>${users.username} : ${users.email} <button class="btn btn-success" onclick=editinfo('${users.username}','${users.email}','${users.contactNo}')>Edit</button> <button class="btn btn-danger" onclick=deleteinfo('${users.email}')>Delete</button></li>`
+    const li = `<li class="list-group-item" id='${users._id}'>${users.username} : ${users.email} <button class="btn btn-success btn-sm" onclick=editinfo('${users.username}','${users.email}','${users.contactNo}')>Edit</button> <button class="btn btn-danger btn-sm" onclick=deleteinfo('${users._id}')>Delete</button></li>`
     const parentNode = document.getElementById('listofusers')
     parentNode.innerHTML += li
 }
@@ -49,8 +49,10 @@ function editinfo(editNameInfo, editEmailInfo, editContactInfo){
 }
 function deleteinfo(delUserInfo){
     // console.log(delUserInfo)
-    localStorage.removeItem(delUserInfo)
-    removeUserInfoFromScreen(delUserInfo);
+    // localStorage.removeItem(delUserInfo);
+    axios.delete(`https://crudcrud.com/api/01d3733d06b14a139325aae2e5514053/AppoinmentData/${delUserInfo}`)
+    .then((response)=>removeUserInfoFromScreen(delUserInfo))
+    .catch((err)=document.body.innerHTML+=`${err}`)
 }
 function removeUserInfoFromScreen(usr){
     const parentNode = document.getElementById('listofusers')
